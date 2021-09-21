7c2b3332990bad7c2c3a8e4c076b6ad5e77c3878
KhanhPV99 <khanhpv801@gmail.com>
2021-09-21 09:48:18 +0700

    Chapter 7: Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
-    get  :home, to: "static_pages#home"
-    get  :help, to: "static_pages#help"
+    get :home, to: "static_pages#home"
+    get :help, to: "static_pages#help"
+
+    resources :users, only: %i(new create show)
   end
 end
